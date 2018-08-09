1c88a79296a83d874823f94b166837338efedebc
JMA <jacobartz1987@gmail.com>
2018-08-09 15:57:27 -0600

    timer functionality, restart functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@ var incorrect = 0;
 var cAnswer = null;
 var pAnswer = null;
 var i = 0;
-
+var interval = null;
 var questions = [
     {
         q: "What color is blue?",
@@ -21,21 +21,55 @@ var questions = [
         ca: "paint"
     }
 ];
-
-$("#start-button").on("click", function () {
+$(".functions").on("click", "#start-button", function () {
     gameLoad();
-    timer();
+    
 })
-$("#restart-button").on("click", function () {
+$("#start-button").on("click",  function () {
     gameLoad();
-    timer();
+    
 })
+$(".functions").on("click", "#restart-button", function () {
 
+    reStart();
+    
+})
+var counter= 8;
+function endGame() {
+    $("#the-timer").text("");
+    $("#results").text("You guessed " + correct +" correctly and " + incorrect + " incorrectly");
+        $("#g-over").text("GAME OVER!");
+        $("#restart-button").remove();
+        $(".functions").append('<a class="btn btn-primary btn-lg" id="restart-button" role="button">Restart!</a>');
+}
+
+function run() {
+    clearInterval(interval)
+    interval = setInterval(timer, 1000);
+    
+}
    
-// function timer(number) {
-//     
-// }
-
+function timer() {
+    
+    counter--;
+    $("#the-timer").text(counter)
+    if (counter === 0) {
+        endGame();
+    }
+}
+function reStart() {
+    clearInterval(interval);
+    i = 0;
+    $("#restart-button").remove();
+    $("#the-question").text('');
+    $("#an-a").html('');
+    $("#an-b").html('');
+    $("#an-c").html('');
+    $("#results").text("");
+    $("#g-over").text("");
+    $(".functions").append('<a class="btn btn-primary btn-lg" id="start-button" role="button">Start!</a>');
+    
+}
 function gameLoad() {
     $("#start-button").remove();
     i = 0
@@ -43,17 +77,19 @@ function gameLoad() {
 }
 
 function qaFill() {
-
+    clearInterval(interval);
     $("#the-question").text(questions[i].q);
     $("#an-a").html(questions[i].a[0]);
     $("#an-b").html(questions[i].a[1]);
     $("#an-c").html(questions[i].a[2]);
-    timer()
+    run();
 }
 
-
+//I know this could be done A LOT more cleanly.
 $("#an-a").on("click", function () {
-    pAnswer = questions[i].a[0]
+    // clearInterval(interval);
+    counter = 8;
+    pAnswer = questions[i].a[0];
     cAnswer = questions[i].ca;
     if (pAnswer === cAnswer) {
         correct++;
@@ -68,14 +104,16 @@ $("#an-a").on("click", function () {
         qaFill()
     }
     if (correct + incorrect === 3) {
-        window.location = "winscreen.html";
+        endGame();
        
     }
 
 })
 
 $("#an-b").on("click", function () {
-    pAnswer = questions[i].a[1]
+    
+    counter = 8;
+    pAnswer = questions[i].a[1];
     cAnswer = questions[i].ca;
     if (pAnswer === cAnswer) {
         correct++;
@@ -87,16 +125,19 @@ $("#an-b").on("click", function () {
         $("#c-or-i").text(" incorrectly.");
     }
     if (correct + incorrect != 3) {
+        // clearInterval(interval);
         qaFill()
     }
     if (correct + incorrect === 3) {
-        window.location = "winscreen.html";
+        endGame();
         
     }
 })
 
 $("#an-c").on("click", function () {
-    pAnswer = questions[i].a[2]
+    
+    counter = 8;
+    pAnswer = questions[i].a[2];
     cAnswer = questions[i].ca;
     if (pAnswer === cAnswer) {
         correct++;
@@ -108,10 +149,11 @@ $("#an-c").on("click", function () {
         $("#c-or-i").text(" incorrectly.");;
     }
     if (correct + incorrect != 3) {
+        // clearInterval(interrval);
         qaFill()
     }
     if (correct + incorrect === 3) {
-        window.location = "winscreen.html";
+        endGame();
         
     }
 })
