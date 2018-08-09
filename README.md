12c2f809e8cab6ca76e4dd4bcaa6cb4547a654ae
JMA <jacobartz1987@gmail.com>
2018-08-08 18:53:32 -0600

    Buttons and game over screen added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TriviaGame
 A game of trivia
+
+
+bugs - no timer
+     - does not print guesses at end screen
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,40 +2,118 @@ var correct = 0;
 var incorrect = 0;
 var cAnswer = null;
 var pAnswer = null;
-var questions = [
-    { q: "What color is blue?",
-      a: ["blue","red","green"],
-      ca: "blue"},
-    { q: "What is rice?",
-      a: ["a legume","a grain","a bean"],
-      ca: "a grain"},
-    { q: "You paint with ____",
-      a: ["charcoal", "graphite", "paint"],
-      ca: "paint" }
-  ];
+var i = 0;
 
-$("#start-button").on("click", function() {
+var questions = [
+    {
+        q: "What color is blue?",
+        a: ["blue", "red", "green"],
+        ca: "blue"
+    },
+    {
+        q: "What is rice?",
+        a: ["a legume", "a grain", "a bean"],
+        ca: "a grain"
+    },
+    {
+        q: "You paint with ____",
+        a: ["charcoal", "graphite", "paint"],
+        ca: "paint"
+    }
+];
+
+$("#start-button").on("click", function () {
     gameLoad();
+    timer();
 })
+$("#restart-button").on("click", function () {
+    gameLoad();
+    timer();
+})
+
+   
+// function timer(number) {
+//     
+// }
 
 function gameLoad() {
     $("#start-button").remove();
-    $(".jumbotron").append('<a class="btn btn-primary btn-lg" id="start-button" role="button">ReStart!</a>');
-    formFill();
+    i = 0
+    qaFill();
 }
 
 function qaFill() {
-    $("#the-question").text(questions[0].q);
-    $("#an-a").html(questions[0].a[0]);
-    $("#an-b").html(questions[0].a[1]);
-    $("#an-c").html(questions[0].a[2]);
-    cAnswer = questions[0].ca;
-    pAnswer = "//onclick";
-    if (pAnswer === cAnswer) {
-        correct++
-        //some kind of clear so the Q&A can be reset
-        qaFill();
-    }
+
+    $("#the-question").text(questions[i].q);
+    $("#an-a").html(questions[i].a[0]);
+    $("#an-b").html(questions[i].a[1]);
+    $("#an-c").html(questions[i].a[2]);
+    timer()
 }
+
+
+$("#an-a").on("click", function () {
+    pAnswer = questions[i].a[0]
+    cAnswer = questions[i].ca;
+    if (pAnswer === cAnswer) {
+        correct++;
+        i++;
+        $("#c-or-i").text(" correctly.");
+    } else {
+        i++;
+        incorrect++;
+        $("#c-or-i").text(" incorrectly.");
+    }
+    if (correct + incorrect != 3) {
+        qaFill()
+    }
+    if (correct + incorrect === 3) {
+        window.location = "winscreen.html";
+       
+    }
+
+})
+
+$("#an-b").on("click", function () {
+    pAnswer = questions[i].a[1]
+    cAnswer = questions[i].ca;
+    if (pAnswer === cAnswer) {
+        correct++;
+        i++;
+        $("#c-or-i").text(" correctly.");
+    } else {
+        i++;
+        incorrect++;
+        $("#c-or-i").text(" incorrectly.");
+    }
+    if (correct + incorrect != 3) {
+        qaFill()
+    }
+    if (correct + incorrect === 3) {
+        window.location = "winscreen.html";
+        
+    }
+})
+
+$("#an-c").on("click", function () {
+    pAnswer = questions[i].a[2]
+    cAnswer = questions[i].ca;
+    if (pAnswer === cAnswer) {
+        correct++;
+        i++;
+        $("#c-or-i").text(" correctly.");
+    } else {
+        i++;
+        incorrect++;
+        $("#c-or-i").text(" incorrectly.");;
+    }
+    if (correct + incorrect != 3) {
+        qaFill()
+    }
+    if (correct + incorrect === 3) {
+        window.location = "winscreen.html";
+        
+    }
+})
 
 
